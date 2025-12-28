33a6468d2d355110c776a97898d6a089e97fd844
Psionik K <73710933+psionic-k@users.noreply.github.com>
2025-12-28 13:37:00 +0900

    envrc: wayland environment & multiple dev shell structure
    Beginning to provide different shells for different platforms and runtime
    environments.  Some may be re-used for building distributable binaries.

--- flake.nix
+++ flake.nix
@@ -9,46 +9,87 @@
       system = "x86_64-linux";
       pkgs = import nixpkgs { inherit system; };
 
-      x11Shell = pkgs.mkShell {
-        buildInputs = with pkgs; [
-          pkg-config
+      vulkanEnv = ''
+          export VK_LAYER_PATH=${pkgs.vulkan-validation-layers}/share/vulkan/explicit_layer.d
+
+          LD_LIBRARY_PATH=${pkgs.vulkan-loader}/lib:$LD_LIBRARY_PATH
+          LD_LIBRARY_PATH=${pkgs.pipewire}/lib:$LD_LIBRARY_PATH
+          LD_LIBRARY_PATH=${pkgs.llvmPackages.libclang.lib}/lib:$LD_LIBRARY_PATH
+      '';
+      vulkanDeps = with pkgs; [
           vulkan-loader
           vulkan-headers
+          vulkan-tools
+          vulkan-validation-layers
+          shader-slang
+      ];
+
+      pipewireEnv = ''
+        LD_LIBRARY_PATH=${pkgs.pipewire}/lib:$LD_LIBRARY_PATH
+      '';
+      pipewireDeps = with pkgs; [
+          pipewire
+          llvmPackages.libclang.lib
+      ];
+
+      x11Deps = with pkgs; [
           xorg.libX11
           xorg.libXcursor
           xorg.libXi
           libxkbcommon
+      ];
 
-          shader-slang
-
-          pipewire
-          llvmPackages.libclang.lib
-
-          vulkan-loader
-          vulkan-tools
-          vulkan-validation-layers
-        ];
+      x11Shell = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          pkg-config
+        ] ++ pipewireDeps ++ x11Deps ++ vulkanDeps;
 
         # Make sure dynamic linker can find libX11 at runtime
         shellHook = ''
-          LD_LIBRARY_PATH=${pkgs.xorg.libX11}/lib
+          ${vulkanEnv}
+          ${pipewireEnv}
+          LD_LIBRARY_PATH=${pkgs.xorg.libX11}/lib:$LD_LIBRARY_PATH
           LD_LIBRARY_PATH=${pkgs.xorg.libXcursor}/lib:$LD_LIBRARY_PATH
           LD_LIBRARY_PATH=${pkgs.xorg.libXi}/lib:$LD_LIBRARY_PATH
           LD_LIBRARY_PATH=${pkgs.libxkbcommon}/lib:$LD_LIBRARY_PATH
-          LD_LIBRARY_PATH=${pkgs.vulkan-loader}/lib:$LD_LIBRARY_PATH
-          LD_LIBRARY_PATH=${pkgs.pipewire}/lib:$LD_LIBRARY_PATH
-          LD_LIBRARY_PATH=${pkgs.llvmPackages.libclang.lib}/lib:$LD_LIBRARY_PATH
           export LD_LIBRARY_PATH
 
           export LIBCLANG_PATH=${pkgs.llvmPackages.libclang.lib}/lib
+        '';
+      };
 
-          export VK_LAYER_PATH=${pkgs.vulkan-validation-layers}/share/vulkan/explicit_layer.d
+      waylandDeps = with pkgs; [
+        wayland
+        wayland-protocols
+        libxkbcommon
+        libdecor
+        libinput
+      ];
+      waylandShell = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          pkg-config
+        ] ++ pipewireDeps ++ waylandDeps ++ vulkanDeps;
+
+        # Make sure dynamic linker can find libX11 at runtime
+        shellHook = ''
+          ${vulkanEnv}
+          ${pipewireEnv}
+          LD_LIBRARY_PATH=${pkgs.wayland}/lib:$LD_LIBRARY_PATH
+          LD_LIBRARY_PATH=${pkgs.wayland-cursor}/lib:$LD_LIBRARY_PATH
+          LD_LIBRARY_PATH=${pkgs.libinput}/lib:$LD_LIBRARY_PATH
+          LD_LIBRARY_PATH=${pkgs.libxkbcommon}/lib:$LD_LIBRARY_PATH
+          LD_LIBRARY_PATH=${pkgs.libdecor}/lib:$LD_LIBRARY_PATH
+          export LD_LIBRARY_PATH
+
+          export LIBCLANG_PATH=${pkgs.llvmPackages.libclang.lib}/lib
         '';
       };
     in {
       devShells = {
         x86_64-linux = {
-          default = x11Shell;
+          default = waylandShell;
+          wayland = waylandShell;
+          x11 = x11Shell;
         };
       };
     };
