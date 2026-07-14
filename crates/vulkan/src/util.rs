1706355dea96423e6548ad3629f8c2975351651f
Psionik K <73710933+psionic-k@users.noreply.github.com>
2026-07-15 05:33:01 +0900

    vulkan: remove "util" descriptor garbage
    Functions like this are always garbage, but at first, it's at least
    out-of-the-way garbage.

--- crates/vulkan/src/util.rs
+++ crates/vulkan/src/util.rs
@@ -26,38 +26,5 @@ pub fn find_memory_type_index(
             return Some(i);
         }
     }
-
     None
-}
-
-/// Layouts and descriptor management in general are annoying.
-// Probably goes into a dedicated binding module.
-// ROLL move spectrum to new descriptor style
-pub fn descriptor_set_layout(device: &ash::Device) -> Result<vk::DescriptorSetLayout, VulkanError> {
-    let bindings = [
-        vk::DescriptorSetLayoutBinding {
-            binding: 0, // spectrum
-            descriptor_type: vk::DescriptorType::STORAGE_BUFFER,
-            descriptor_count: 1,
-            stage_flags: vk::ShaderStageFlags::COMPUTE,
-            p_immutable_samplers: std::ptr::null(),
-            ..Default::default()
-        },
-        vk::DescriptorSetLayoutBinding {
-            binding: 1, // output buffer
-            descriptor_type: vk::DescriptorType::STORAGE_BUFFER,
-            descriptor_count: 1,
-            stage_flags: vk::ShaderStageFlags::COMPUTE,
-            p_immutable_samplers: std::ptr::null(),
-            ..Default::default()
-        },
-    ];
-
-    let layout_info = vk::DescriptorSetLayoutCreateInfo {
-        binding_count: bindings.len() as u32,
-        p_bindings: bindings.as_ptr(),
-        ..Default::default()
-    };
-
-    Ok(unsafe { device.create_descriptor_set_layout(&layout_info, None)? })
 }
